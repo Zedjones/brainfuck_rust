1f2d2b8e99f2bf7a5b383bf2475a6aa276c11334
Nicholas <dojoman19@gmail.com>
2019-03-06 11:53:08 -0500

    Added more error checking

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,14 @@ pub mod brainfuck {
     use std::fs::File;
     use std::io::{stdin, stdout, Write};
 
-    pub fn process_input() {
+    pub fn process_input() -> Result<(), std::io::Error>{
         let mut line = String::new();
         let _incomplete_loop = false;
         print!("> ");
-        stdout().flush();
+        match stdout().flush() {
+            Ok(_) => (),
+            Err(err) => return Err(err)
+        };
         loop {
             let res = stdin().read_line(&mut line);
             match res {
@@ -20,7 +23,11 @@ pub mod brainfuck {
             println!("{}", line);
             line.clear();
             print!("> ");
-            stdout().flush();
+            match stdout().flush() {
+                Ok(_) => (),
+                Err(err) => return Err(err)
+            };
         }
+        Ok(())
     }
 }
