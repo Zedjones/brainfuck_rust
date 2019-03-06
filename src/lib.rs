73e9cf478f8c940bb9df085bc8569f00f144f6ad
Zedjones <dojoman19@gmail.com>
2019-03-05 23:48:55 -0500

    Small fixed to process_input loop

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,13 @@
 pub mod brainfuck {
 
     use std::fs::File;
-    use std::io::stdin;
+    use std::io::{stdin, stdout, Write};
 
     pub fn process_input() {
         let mut line = String::new();
         let _incomplete_loop = false;
         print!("> ");
+        stdout().flush();
         loop {
             let res = stdin().read_line(&mut line);
             match res {
@@ -15,9 +16,11 @@ pub mod brainfuck {
                 Ok(_) => (),
                 Err(e) => panic!("Failed to read from stdin: {}", e)
             }
+            line = line.trim().to_string();
             println!("{}", line);
             line.clear();
             print!("> ");
+            stdout().flush();
         }
     }
 }
